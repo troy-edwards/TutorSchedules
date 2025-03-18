f5d8e8a915a49d8c4b11e0688b95cadb83b1eaf9
Millerah <troy.edwards@meeseproductions.com>
2025-03-18 11:43:29 -0500

    Added some collapsable logic for custom time on index.

--- TutorSchedules/Pages/Index.cshtml.cs
+++ TutorSchedules/Pages/Index.cshtml.cs
@@ -6,6 +6,14 @@ namespace TutorSchedules.Pages;
 public class IndexModel : PageModel
 {
 	private readonly ILogger<IndexModel> _logger;
+	[BindProperty]
+	public String Subject { get; set; }
+	[BindProperty]
+	public bool UseCustomTime { get; set; }
+	[BindProperty]
+	public TimeOnly CustomTime { get; set; }
+	[BindProperty]
+	public DayOfWeek CustomWeekDay { get; set; }
 
 	public IndexModel(ILogger<IndexModel> logger)
 	{
