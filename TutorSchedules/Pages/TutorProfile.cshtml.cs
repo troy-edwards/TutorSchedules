89a0ade520ec25a1430ed782a2b4f8c3ae67f8e6
Millerah <troy.edwards@meeseproductions.com>
2025-03-19 12:51:42 -0500

    Added a TutorProfile page layout, to handle adding/editing a tutor's information.

---
+++ TutorSchedules/Pages/TutorProfile.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TutorSchedules.Data;
+using TutorSchedules.Models;
+
+namespace TutorSchedules.Pages;
+
+public class TutorInfoModel : PageModel
+{
+	private ScheduleContext _context;
+	[BindProperty(SupportsGet = true)]
+	public int? Id { get; set; }
+	public Tutor Tutor { get; set; }
+	public bool AddingNewTutor { get; set; }
+
+	public TutorInfoModel(ScheduleContext context)
+	{
+		_context = context;
+	}
+
+	public async Task OnGet()
+	{
+		AddingNewTutor = !Id.HasValue;
+		if (AddingNewTutor)
+		{
+			Tutor = new Tutor();
+		}
+			
+			Tutor = await _context.Tutors.FirstOrDefaultAsync(t => t.Id == Id);
+	}
+}
