984624172a13e63cfa19e832293fc1c7dcc80cb0
Millerah <troy.edwards@meeseproductions.com>
2025-03-21 12:24:38 -0500

    Cleaned up ScheduleReader.cs to be more readable.

--- TutorSchedules/Utilities/ScheduleReader.cs
+++ TutorSchedules/Utilities/ScheduleReader.cs
@@ -9,8 +9,9 @@ public class ScheduleReader
     private readonly Dictionary<string, int> _nameToId;
     private readonly List<TimeBlock> _blocks;
     private string _previousName;
-    
-    private static readonly List<string> _offStrings = [
+
+    private static readonly List<string> _offStrings =
+    [
         "Off",
         ""
     ];
@@ -24,50 +25,87 @@ public class ScheduleReader
 
     public List<TimeBlock> ExtractSchedule()
     {
-        _previousName = "";
-        _blocks.Clear();
-        if (_fileLines is null || _fileLines.Count() < 2)
-        {
-            throw new FormatException("The file does not have enough lines to read. (Needs at least a Title and Table Header.)");
-        }
-        
-        for (int i = 2; i < _fileLines.Count(); i++)
-        {
-            ProcessRow(_fileLines.ElementAt(i), i);
-        }
+        ResetData();
+        ConfirmHeaderExists();
+        ProcessRows();
         return _blocks;
     }
 
+    private void ResetData()
+    {
+        _previousName = "";
+        _blocks.Clear();
+    }
+
+    private void ConfirmHeaderExists()
+    {
+        if (_fileLines is null || _fileLines.Count() < 2)
+            throw new FormatException(
+                "The file does not have enough lines to read. (Needs at least a Title and Table Header.)");
+    }
+
+    private void ProcessRows()
+    {
+        for (int i = 2; i < _fileLines.Count(); i++)
+            ProcessRow(_fileLines.ElementAt(i), i);
+    }
+
+
     private void ProcessRow(string row, int rowNumber)
+    {
+        var columns = SplitColumns(row, rowNumber);
+        ConfirmColumnsAreValid(rowNumber, columns);
+        var name = GetNameFromColumn(columns);
+        var tutorId = GetTutorIdFromName(name);
+        ExtractTimesFromColumns(rowNumber, columns, tutorId);
+    }
+
+    private static string[] SplitColumns(string row, int rowNumber)
     {
         if (row is null)
             throw new FormatException($"A row can not be null. Row {rowNumber + 1} is null.");
         var columns = row.Split(",");
-        
+        return columns;
+    }
+
+    private static void ConfirmColumnsAreValid(int rowNumber, string[] columns)
+    {
         if (columns.Length != 7)
-            throw new FormatException($"A row must have 7 columns. Row {rowNumber + 1} has {columns.Length} columns. (Merged columns still count as multiple columns.");
+            throw new FormatException(
+                $"A row must have 7 columns. Row {rowNumber + 1} has {columns.Length} columns. (Merged columns still count as multiple columns.");
+    }
+
+    private string GetNameFromColumn(string[] columns)
+    {
         string name = columns.First();
         name = name == "" ? _previousName : name;
         _previousName = name;
-        
-        if (!_nameToId.ContainsKey(name))
-            throw new FormatException($"{name} is on the schedule, but not in the tutor list. Their display name must match what is on the schedule.");
-        int tutorId = _nameToId[name];
+        return name;
+    }
 
+    private int GetTutorIdFromName(string name)
+    {
+        if (!_nameToId.ContainsKey(name))
+            throw new FormatException(
+                $"{name} is on the schedule, but not in the tutor list. Their display name must match what is on the schedule.");
+        int tutorId = _nameToId[name];
+        return tutorId;
+    }
+
+    private void ExtractTimesFromColumns(int rowNumber, string[] columns, int tutorId)
+    {
         for (DayOfWeek day = DayOfWeek.Monday; day <= DayOfWeek.Saturday; day++)
         {
             int columnNumber = (int)day;
             string timeInterval = columns[columnNumber].Trim();
-            
-            if(TutorIsOff(timeInterval))
+
+            if (TutorIsOff(timeInterval))
                 continue;
-            var startAndEndTimes = timeInterval.Split("-").Select(s => s.Trim()).ToArray();
-            if (startAndEndTimes.Count() != 2)
-                throw new FormatException($"Each cell on the schedule must contain exactly 2 times, seperated by a \"-\". Row {rowNumber + 1}, column {columnNumber + 1} does not meet this requirement.");
+
+            var startAndEndTimes = GetStartAndEndTimesAsStrings(rowNumber, timeInterval, columnNumber);
             var startTime = GetTimeFromString(startAndEndTimes[0], rowNumber, columnNumber);
             var endTime = GetTimeFromString(startAndEndTimes[1], rowNumber, columnNumber);
-            if (endTime < startTime)
-                throw new FormatException($"End time is before start time at row {rowNumber + 1}, column {columnNumber + 1}.");
+            ConfirmTimesAreInOrder(rowNumber, endTime, startTime, columnNumber);
             _blocks.Add(new TimeBlock(tutorId, day, startTime, endTime));
         }
     }
@@ -77,11 +115,28 @@ public class ScheduleReader
         return _offStrings.Contains(timeInterval);
     }
 
+    private static string[] GetStartAndEndTimesAsStrings(int rowNumber, string timeInterval, int columnNumber)
+    {
+        var startAndEndTimes = timeInterval.Split("-").Select(s => s.Trim()).ToArray();
+        if (startAndEndTimes.Count() != 2)
+            throw new FormatException(
+                $"Each cell on the schedule must contain exactly 2 times, seperated by a \"-\". Row {rowNumber + 1}, column {columnNumber + 1} does not meet this requirement.");
+        return startAndEndTimes;
+    }
+
     private TimeOnly GetTimeFromString(string timeString, int row, int column)
     {
         bool parseSuccessful = TimeOnly.TryParse(timeString, out TimeOnly time);
         if (!parseSuccessful)
-            throw new FormatException($"Times must be in the format \"XX:XX XM\". \"{timeString}\" at row {row + 1}, column {column + 1} was not read.");
+            throw new FormatException(
+                $"Times must be in the format \"XX:XX XM\". \"{timeString}\" at row {row + 1}, column {column + 1} was not read.");
         return time;
+    }
+
+    private static void ConfirmTimesAreInOrder(int rowNumber, TimeOnly endTime, TimeOnly startTime, int columnNumber)
+    {
+        if (endTime < startTime)
+            throw new FormatException(
+                $"End time is before start time at row {rowNumber + 1}, column {columnNumber + 1}.");
     }
 }
