1a17351f301b5de4694b3dd527acc2e27fbbc8a9
Millerah <troy.edwards@meeseproductions.com>
2025-03-21 20:32:27 -0500

    Added clear schedule to settings page. Also added some missing page titles I found.

---
+++ TutorSchedules/Pages/ScheduleDeleted.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TutorSchedules.Pages;
+
+public class ScheduleDeleted : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- TutorSchedules/Pages/Settings.cshtml.cs
+++ TutorSchedules/Pages/Settings.cshtml.cs
@@ -10,67 +10,74 @@ namespace TutorSchedules.Pages;
 
 public class SettingsModel : PageModel
 {
-	[BindProperty]
-	public IFormFile ScheduleFile { get; set; }
+    [BindProperty] public IFormFile ScheduleFile { get; set; }
 
-	private ScheduleContext _context;
+    private ScheduleContext _context;
 
-	public SettingsModel(ScheduleContext context)
-	{
-		_context = context;
-	}
+    public SettingsModel(ScheduleContext context)
+    {
+        _context = context;
+    }
 
-	public void OnGet()
-	{
-	}
+    public void OnGet()
+    {
+    }
 
-	public async Task<IActionResult> OnPostUploadScheduleAsync()
-	{
-		if (ScheduleFile is null)
-		{
-			ModelState.ClearValidationState("ScheduleFile");
-			ModelState.AddModelError("ScheduleFile", "Please upload a file.");
-			return Page();
-		}
-		if (ScheduleFile.ContentType != "text/csv")
-		{
-			ModelState.AddModelError("ScheduleFile", "The file must be a csv file.");
-			return Page();
-		}
-		using( var reader = new StreamReader(ScheduleFile.OpenReadStream()))
-		{
-			var nameToId = new Dictionary<string, int>();
-			await _context.Tutors.ForEachAsync(t => nameToId.TryAdd(t.DisplayName, t.Id));
-			var rows = new List<string>();
-			string? row = await reader.ReadLineAsync();
-			while (row is not null)
-			{
-				rows.Add(row);
-				row = await reader.ReadLineAsync();
-			}
-			ScheduleReader scheduleReader = new ScheduleReader(rows, nameToId);
-			try
-			{
-				var blocks= scheduleReader.ExtractSchedule();
-				_context.ScheduleBlocks.RemoveRange(_context.ScheduleBlocks);
-				foreach (var block in blocks)
-				{
-					_context.ScheduleBlocks.Add(block);
-				}
-				
-				int tutorsAdded = blocks.GroupBy(b => b.Tutor).Count();
-				int blocksAdded = blocks.Count;
-				await _context.SaveChangesAsync();
-				return RedirectToPage($"/UploadSuccessful", new { TutorsAdded = tutorsAdded, BlocksAdded = blocksAdded});
-			}
-			catch (FormatException e)
-			{
-				ModelState.AddModelError("ScheduleFile", e.Message);
-				return Page();
-			}
-			
-		}
-	}
+    public async Task<IActionResult> OnPostUploadScheduleAsync()
+    {
+        if (ScheduleFile is null)
+        {
+            ModelState.ClearValidationState("ScheduleFile");
+            ModelState.AddModelError("ScheduleFile", "Please upload a file.");
+            return Page();
+        }
 
+        if (ScheduleFile.ContentType != "text/csv")
+        {
+            ModelState.AddModelError("ScheduleFile", "The file must be a csv file.");
+            return Page();
+        }
 
+        using (var reader = new StreamReader(ScheduleFile.OpenReadStream()))
+        {
+            var nameToId = new Dictionary<string, int>();
+            await _context.Tutors.ForEachAsync(t => nameToId.TryAdd(t.DisplayName, t.Id));
+            var rows = new List<string>();
+            string? row = await reader.ReadLineAsync();
+            while (row is not null)
+            {
+                rows.Add(row);
+                row = await reader.ReadLineAsync();
+            }
+
+            ScheduleReader scheduleReader = new ScheduleReader(rows, nameToId);
+            try
+            {
+                var blocks = scheduleReader.ExtractSchedule();
+                _context.ScheduleBlocks.RemoveRange(_context.ScheduleBlocks);
+                foreach (var block in blocks)
+                {
+                    _context.ScheduleBlocks.Add(block);
+                }
+
+                int tutorsAdded = blocks.GroupBy(b => b.Tutor).Count();
+                int blocksAdded = blocks.Count;
+                await _context.SaveChangesAsync();
+                return RedirectToPage($"/UploadSuccessful",
+                    new { TutorsAdded = tutorsAdded, BlocksAdded = blocksAdded });
+            }
+            catch (FormatException e)
+            {
+                ModelState.AddModelError("ScheduleFile", e.Message);
+                return Page();
+            }
+        }
+    }
+
+    public async Task<IActionResult> OnPostDeleteScheduleAsync()
+    {
+        _context.ScheduleBlocks.RemoveRange(_context.ScheduleBlocks);
+        await _context.SaveChangesAsync();
+        return RedirectToPage("/ScheduleDeleted");
+    }
 }
