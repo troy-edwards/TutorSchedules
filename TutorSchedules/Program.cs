9b34e37abb4168c0e4b31a0ee203bd1ce1b3b78c
Millerah <troy.edwards@meeseproductions.com>
2025-03-24 21:48:25 -0500

    Made app get the production connection string from the environment variable.

--- TutorSchedules/Program.cs
+++ TutorSchedules/Program.cs
@@ -5,10 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-//builder.Services.
-builder.Services.AddDbContext<ScheduleContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDbContext<ScheduleContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString(
+        builder.Environment.IsDevelopment() ? 
+        "DefaultConnection" : 
+        Environment.GetEnvironmentVariable("SQLAZURECONNSTR_")
+        )));
 
 
 var app = builder.Build();
@@ -19,9 +22,9 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
